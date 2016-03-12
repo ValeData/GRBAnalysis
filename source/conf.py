033981a1acee0f2e885e9d34ff320b645c9499bb
Y.Wang <yu@shuil.org>
2016-03-12 03:13:55 +0100

    update cons file

--- source/conf.py
+++ source/conf.py
@@ -208,7 +208,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'GRBAnalysisdoc'
+htmlhelp_basename = 'Learn GRB Data Analysis in 3 Days'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -230,8 +230,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'GRBAnalysis.tex', 'GRBAnalysis Documentation',
-     'Vale Data (Y.Wang ...)', 'manual'),
+    (master_doc, 'GRBAnalysis.tex', 'Learn GRB Data Analysis in 3 Days',
+     '(Y.Wang, M.Peresano, R.Moradi, S.Shakeri, Y.Aimuratov)', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -260,7 +260,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'grbanalysis', 'GRBAnalysis Documentation',
+    (master_doc, 'grbanalysis', 'Learn GRB Data Analysis in 3 Days',
      [author], 1)
 ]
 
@@ -274,8 +274,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'GRBAnalysis', 'GRBAnalysis Documentation',
-     author, 'GRBAnalysis', 'One line description of project.',
+    (master_doc, 'GRBAnalysis', 'Learn GRB Data Analysis in 3 Days',
+     author, 'GRBAnalysis', 'A Short and Practical Guide.',
      'Miscellaneous'),
 ]
 

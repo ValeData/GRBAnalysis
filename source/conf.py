42a5407876f24cf1ee20d00d3d6f34bbf54ae1ed
Y.Wang <yu@shuil.org>
2016-03-13 04:38:45 +0100

    comment some extensions

--- source/conf.py
+++ source/conf.py
@@ -31,10 +31,10 @@ import sphinx_rtd_theme
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
+#    'sphinx.ext.autodoc',
+#    'sphinx.ext.intersphinx',
+#    'sphinx.ext.todo',
+#    'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
 ]
